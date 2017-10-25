c7699605fa1e3960bec3f5c0baede874fef6e358
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2017-10-25 20:34:20 +0200

    Fix regex

--- test.rb
+++ test.rb
@@ -1,6 +1,11 @@
+# rubocop:disable all
 String
 Some::Class
 {class: nil}
 {:class => nil}
 class Model
 end
+"benoit".match?(/ben/)
+"benoit".match(/ben/)
+"benoit".match? /ben/
+"benoit".match /ben/
